16420a75e8ca94f170ee615617f5b44947afecf2
Kushm <kbanbah@yahoo.com>
2020-11-27 13:11:28 +0530

    scripts

---
+++ javascript/movies.js
@@ -0,0 +1,71 @@
+function getMovies() {
+    return [
+        {
+            id:1,
+            type:"now",
+            thumbnail:"images/barbecue.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
+            name:"The Barbecue Movie",
+            cast:"Steak, Grill, Electricity",
+            director:"Steak Baker",
+            genre:"Hot, Grilling",
+            duration: 130
+        },
+        {
+            id:2,
+            type:"now",
+            thumbnail:"images/bigbuck.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+            name:"The Big Buck",
+            cast:"Deers, Plants, Trees, Sun",
+            director:"Hunter Survivor",
+            genre:"Relaxing,Green",
+            duration: 123
+        },
+        {
+            id:3,
+            type:"now",
+            thumbnail:"images/chocolate.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+            name:"The Chocolate Movie",
+            cast:"Flour, Chocolate, Hersheys",
+            director:"French Baker",
+            genre:"Obesity, Tasty, Scrumptious",
+            duration: 40
+        },
+        {
+            id:4,
+            type:"upcoming",
+            thumbnail:"images/nature.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name:"The Nature Movie",
+            cast:"Birds, Animals, Humans, Sharks",
+            director:"David Attenborough",
+            genre:"Natural, Windy, Informative",
+            duration: 150
+        },
+        {
+            id:5,
+            type:"upcoming",
+            thumbnail:"images/wildlife.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name:"The Landscape Movie",
+            cast:"Rocks, Rivers, Clouds, Skies",
+            director:"Walter Mitty",
+            genre:"Rocky, Dramatic, Suspenseful",
+            duration: 120
+        },
+        {
+            id:6,
+            type:"upcoming",
+            thumbnail:"images/wonders.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+            name:"The Wonderful Movie",
+            cast:"Egyptians, Bablyonians, Mughal King and Wives",
+            director:"Khufu",
+            genre:"Desert, Awe, Inspiring",
+            duration: 120
+        }
+        
+    ]
+}
